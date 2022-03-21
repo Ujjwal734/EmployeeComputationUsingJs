a8976b55593cd7fd3d4bf458fb25d36a3e49f36e
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-03-21 23:12:34 +0530

    Checking Employee Present Absent

--- Employeejs.js
+++ Employeejs.js
@@ -1,1 +1,13 @@
 console.log("Hello TO Js Code...! ");
+
+//UC1 = EMPLOYEE PRESENT ABSENT
+const IS_ABSENT = 0
+let employCheck = Math.floor(Math.random() * 10) % 2;
+if (employCheck == IS_ABSENT)
+{
+    console.log("Employee is Present");
+}
+else
+{
+    console.log("Employee is Absent")
+}
